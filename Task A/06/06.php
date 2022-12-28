92369c0d39ddde127576b7cba1cc98457140f64f
ho0527 <chris960527ho@gmail.com>
2022-12-28 21:07:46 +0800

    20221228

--- Task A/06/06.php
+++ Task A/06/06.php
@@ -15,14 +15,18 @@
         </form>
         <?php
             session_start();
-            if (isset($_GET['captcha_code'])) {
-            // Check if the captcha code is correct
-                if ($_GET['captcha_code'] == $_SESSION['captcha_code']) {
-                    // Captcha code is correct
-                    echo "<p style='font-size: 48px; font-weight: bold; color: #008800'>成功</p>";
-                } else {
-                    // Captcha code is incorrect
-                    echo "<p style='font-size: 48px; font-weight: bold; color: #880000'>失敗</p>";
+            if(isset($_GET["captcha_code"])){
+                //判斷輸入值是否與SESSION值相同
+                if($_GET["captcha_code"]==$_SESSION["captcha_code"]){
+                    //做成功圖片
+                    ?>
+                    <p style="font-size:48px;font-weight:bold;color:#008800">成功</p>
+                    <?php
+                }else{
+                    // 做失敗圖片
+                    ?>
+                    <p style="font-size:48px;font-weight:bold;color:#880000">失敗</p>
+                    <?php
                 }
             }
         ?>
---
+++ Task A/10/myScript.js
@@ -0,0 +1,15 @@
+let a=document.getElementById("a")
+let b=document.getElementById("b")
+let c=document.getElementById("c")
+
+a.onclick=function(){
+    location.href="a.html"
+}
+
+b.onclick=function(){
+    location.href="b.html"
+}
+
+c.onclick=function(){
+    location.href="c.html"
+}
