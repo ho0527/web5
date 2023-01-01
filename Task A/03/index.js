ea3c88143c056d6bc77f3ae9c859438d867c84ee
ho0527 <chris960527ho@gmail.com>
2023-01-01 16:33:07 +0800

    20230101 A3

--- Task A/03/index.js
+++ Task A/03/index.js
@@ -1,42 +1,36 @@
 let mod=document.getElementById("change")
 let buttons=document.querySelectorAll('button')
 let output=document.getElementById("output")
+let a="all"
 //訂定變數
 
 output.value=""//將output清空
 
 //變換value
 mod.onclick=function(){
-	if(mod.value=="後兩位模式"){
-		mod.value="全部模式"
-	}else{
+	if(mod.value=="全部模式"){
 		mod.value="後兩位模式"
+		a="twodecimals"
+	}else{
+		mod.value="全部模式"
+		a="all"
 	}
 }
 
-if(mod.value=="後兩位模式"){
-	buttons.forEach(function(button){
-		button.addEventListener('click',function(){
-			if (this.id=='c') {
-				output.value=""//將output清空
-			} else if(this.id=='calc='){
+buttons.forEach(function(button){
+	button.addEventListener('click',function(){
+		if (this.id=='c') {
+			output.value=""//將output清空
+		} else if(this.id=='calc='){
+			console.log("eq"+a)
+			if(a=="twodecimals"){//判斷a是否為twodecimals
 				let result=eval(output.value)
 				output.value=Math.round(result * 100) / 100;//四捨五入
-			} else {
-				output.value=output.value+this.textContent//加字串
-			}
-		})
-	})
-}else{
-	buttons.forEach(function(button){
-		button.addEventListener('click',function(){
-			if (this.id=='c') {
-				output.value=""//將output清空
-			} else if(this.id=='calc='){
+			}else{
 				output.value=eval(output.value)//印出結果
-			} else {
-				output.value=output.value+this.textContent//加字串
 			}
-		})
+		} else {
+			output.value=output.value+this.textContent//加字串
+		}
 	})
-}
+})
