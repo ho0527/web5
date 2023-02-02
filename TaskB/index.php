7fb67b0b84a9cd0c50ac7c807e4ec6d8f113a6a8
ho0527 <chris960527ho@gmail.com>
2023-02-02 15:09:20 +0800

    20230202

--- TaskB/index.php
+++ TaskB/index.php
@@ -14,5 +14,25 @@
             <button class="button"></button>
             <button class="button"></button>
         </div>
+        <?php
+            $a=[];
+            $count=0;
+            while($count<7){
+                $num=rand(1,50);
+                array_push($a,$num);
+                if($count>0){
+                    for($i=0;$i<=$count;$i=$i+1){;
+                        // if($a[$i]==$num){
+                        //     array_pop($a);
+                        //     $count=$count-1;
+                        // }
+                    }
+                }
+                $count++;
+            }
+            for($i=0;$i<count($a);$i++){
+                echo($a[$i]." ");
+            }
+        ?>
     </body>
 </html>
